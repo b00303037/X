c1271360af8e13940bd98f7ca60f0417a26c4427
klearthinkdev <dev@klearthink.com>
2024-04-12 11:47:11 +0800

    feat: configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// pages
+import { HomeComponent } from './pages/home/home.component';
+import { SubscriptionComponent } from './pages/home/subscription/subscription.component';
+import { ServiceComponent } from './pages/home/service/service.component';
+import { AdminComponent } from './pages/admin/admin.component';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'subscription',
+        component: SubscriptionComponent,
+      },
+      {
+        path: 'service',
+        component: ServiceComponent,
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+  },
+];
